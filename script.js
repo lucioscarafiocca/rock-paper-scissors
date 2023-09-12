22d13dfdc1ab6191172e57d26c213ee41f147ff2
lucio <141471494+lucioscarafiocca@users.noreply.github.com>
2023-09-11 22:18:39 -0300

    finish bare bones js css missing

--- script.js
+++ script.js
@@ -25,113 +25,91 @@ function getComputerChoice() {
 
 
 
-function playerSelection() {
-    let a = prompt("Choose either rock, paper or scissors")
-    let b = a.toLowerCase()
-    if (b = "rock") {
-        return rock;
-    } else if (b = "paper") {
-        return paper;
-    } else {
-        return scissors;
-    }
-    
-    
-}
 
-function playRound () {
+
+function playRound (a) {
  let comp = getComputerChoice();
- let human = playerSelection();
+ let human = a
  let result;
 
- if (human == paper && comp == scissors) {
+
+ 
+
+ if (human == "paper" && comp == scissors) {
     result = "You lost! Scissors beats Paper";
- } else if (human == paper && comp == rock) {
+ } else if (human == "paper" && comp == rock) {
     result = "You won! Paper beats Rock"
- } else if (human == paper && comp == paper) {
+ } else if (human == "paper" && comp == paper) {
     result = "You tied with Paper! Go again!"
- }  else if (human == rock && comp == paper) {
+ }  else if (human == "rock" && comp == paper) {
     result = "You lost! Paper beats Rock"
- } else if (human == rock && comp == scissors) {
+ } else if (human == "rock" && comp == scissors) {
     result = "You won! Rock beats Scissors"
- } else if (human == rock && comp == rock) {
+ } else if (human == "rock" && comp == rock) {
     result = "You tied with Rock! Go again!"
- } else if (human == scissors && comp == rock) {
+ } else if (human == "scissors" && comp == rock) {
     result = "You lost! Rock beats Scissors"
- } else if (human == scissors && comp == paper) {
+ } else if (human == "scissors" && comp == paper) {
     result = "You won! Scissors beats Paper"
  } else { result = "You tied with Scissors! Go again!"}
+ 
+ const p = document.querySelector(".result")
+ p.textContent = result;
   
  return result;
 }
 
 
-function game() {
+function game(a) {
 
-let decider;
-let result;
-let win = 0
-let lose = 0
-let tie = 0 
-    
-    
-
-
- console.log(playRound())
- if (result == "You lost! Scissors beats Paper" || "You lost! Paper beats Rock" || "You lost! Rock beats Scissors") {
-    win++
-  } else if (result == "You won! Paper beats Rock" || "You won! Rock beats Scissors" || "You won! Scissors beats Paper") {
+   let value = playRound(a)
+ 
+  
+  if (value === "You lost! Scissors beats Paper" || value === "You lost! Paper beats Rock" || value === "You lost! Rock beats Scissors") {
     lose++
-  } else {
-    tie++
-  }
- console.log(playRound())
- if (result == "You lost! Scissors beats Paper" || "You lost! Paper beats Rock" || "You lost! Rock beats Scissors") {
+  } else if (value === "You won! Paper beats Rock" || value === "You won! Rock beats Scissors" || value === "You won! Scissors beats Paper") {
     win++
-  } else if (result == "You won! Paper beats Rock" || "You won! Rock beats Scissors" || "You won! Scissors beats Paper") {
-    lose++
-  } else {
-    tie++
-  }
- console.log(playRound())
- if (result == "You lost! Scissors beats Paper" || "You lost! Paper beats Rock" || "You lost! Rock beats Scissors") {
-    win++
-  } else if (result == "You won! Paper beats Rock" || "You won! Rock beats Scissors" || "You won! Scissors beats Paper") {
-    lose++
-  } else {
-    tie++
-  }
- console.log(playRound())
- if (result == "You lost! Scissors beats Paper" || "You lost! Paper beats Rock" || "You lost! Rock beats Scissors") {
-    win++
-  } else if (result == "You won! Paper beats Rock" || "You won! Rock beats Scissors" || "You won! Scissors beats Paper") {
-    lose++
-  } else {
-    tie++
-  }
- console.log(playRound())
- if (result == "You lost! Scissors beats Paper" || "You lost! Paper beats Rock" || "You lost! Rock beats Scissors") {
-    win++
-  } else if (result == "You won! Paper beats Rock" || "You won! Rock beats Scissors" || "You won! Scissors beats Paper") {
-    lose++
   } else {
     tie++
   }
 
+ 
+  const WIN = document.querySelector(".win") 
+  const TIE = document.querySelector(".tie")  
+  const LOSE= document.querySelector(".lose")
+ 
+  WIN.textContent = win
+  LOSE.textContent = lose
+  TIE.textContent = tie;
 
- if (win >= 3) {
-    decider = "You won! Thanks for playing!"
- } else if (lose >= 3) {
-    decider = "You lost! Better luck nex time!"
- } else {
-    decider = "Its a tie!!"
- }
- return decider;
+
+const p = document.querySelector(".final-result")
+p.textContent = decider;
+
+  if (win == 5) {
+   decider = "You won! Thanks for playing!";
+} else if (lose == 5) {
+   decider = "You lost! Better luck nex time!";
+} else {}
 
 }
+   let decider;
+   let win = 0
+   let lose = 0
+   let tie = 0 
+   const rockPick = document.querySelector(".rock")
+   const paperPick = document.querySelector(".paper")
+   const scissorsPick = document.querySelector(".scissors")
+
+   rockPick.addEventListener("click", () => { game("rock")})
+   paperPick.addEventListener("click", () => { game("paper")})
+   scissorsPick.addEventListener("click", () => { game("scissors")})
+   
+   
 
 
-console.log (game())
+
+
 
 
 
